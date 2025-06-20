51c8bc049429d0e559b40ffba50ea98fd63bb36a
Josue Zelaya <josuen3ds@gmail.com>
2025-06-20 08:23:46 -0600

    Modo manual de la aplicacion

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-from datetime import date, timedelta
-from apscheduler.schedulers.blocking import BlockingScheduler
+from datetime import date, datetime, timedelta
+#from apscheduler.schedulers.blocking import BlockingScheduler
 from src.api.app import process_emails
-
-# def job():
-#     today = date.today()
-#     start = (today - timedelta(days=1)).strftime("%Y-%m-%d")
-#     end   =  today          .strftime("%Y-%m-%d")
-#     process_emails(start, end)
 
 def job():
     today = date.today()
@@ -16,19 +10,29 @@ def job():
     process_emails(start_date, end_date)
 
 
-# if __name__ == "__main__":
-#     sched = BlockingScheduler(timezone="America/El_Salvador")
-#     # Ejecutar cada día a la 1:00 AM
-#     sched.add_job(job, 'cron', hour=1, minute=0)
-#     sched.start()
-
 if __name__ == "__main__":
     # Ejecuta el proceso automáticamente con fechas actuales
     job()
 
+# def validar_fecha(fecha_str):
+#     try:
+#         return datetime.strptime(fecha_str, "%Y-%m-%d").date()
+#     except ValueError:
+#         print(f"ERROR: La fecha '{fecha_str}' no tiene el formato correcto 'YYYY-MM-DD' o no es una fecha válida.")
+#         return None
+
 # if __name__ == "__main__":
-#     # Ajusta aquí las fechas para testear
-#     start_date = "2025-06-14"
-#     end_date   = "2025-06-15"
-#     print(f"Ejecutando proceso manual: {start_date} → {end_date}")
-#     process_emails(start_date, end_date)
+#     print("Ingrese las fechas en formato 'YYYY-MM-DD'")
+
+#     start_input = input("Fecha Inicio: ")
+#     end_input = input("Fecha Fin: ")
+
+#     start_date = validar_fecha(start_input)
+#     end_date   = validar_fecha(end_input)
+
+#     if start_date and end_date:
+#         end_date_adjusted = end_date + timedelta(days=1)
+#         print(f"Ejecutando proceso manual: {start_date} → {end_date_adjusted}")
+#         process_emails(start_date.strftime("%Y-%m-%d"), end_date_adjusted.strftime("%Y-%m-%d"))
+#     else:
+#         print("Proceso cancelado por error en las fechas ingresadas.")
